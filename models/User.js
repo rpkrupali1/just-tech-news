71627c51ad547857b083c0d39524ebee3af16069
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-21 04:23:29 -0600

    hashed password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const {Model , DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create user model
 class User extends Model {}
@@ -37,6 +38,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData){
+                newUserData.password = await bcrypt.hash(newUserData.password,10);
+                return newUserData;                
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         // table configuration options
         sequelize,
         timestamps: false,
