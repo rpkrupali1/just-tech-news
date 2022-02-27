7c7dc8c1bdcf9faa4a96bf338fb08a18fa7c0ae1
krupali pilgulwar <rpkrupali@gmail.com>
2022-02-27 16:05:30 -0600

    authentication added

--- server.js
+++ server.js
@@ -1,20 +1,33 @@
-const express = require("express");
-const routes = require("./controllers");
-const sequelize = require("./config/connection");
 const path = require("path");
-const exphbs = require('express-handlebars');
+const express = require("express");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
 const hbs = exphbs.create({});
+const session = require('express-session');
+const SequelizeStore  = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
+app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 app.use(express.static(path.join(__dirname, "public")));
+app.use(require('./controllers/'));
+
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
